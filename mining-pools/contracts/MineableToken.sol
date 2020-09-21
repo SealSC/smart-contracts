737550e1a46e9d2161dbc9921dfe763d9b692d9d
AKACoder <akacoder@outlook.com>
2020-09-21 21:27:52 +0800

    separate different mineable token contracts

--- mining-pools/contracts/MineableToken.sol
+++ mining-pools/contracts/MineableToken.sol
@@ -6,22 +6,16 @@ import "./IMineableToken.sol";
 
 contract MineableToken is IMineableToken, ERC20, ERC20Detailed, Ownable {
     using SafeMath for uint256;
-    address public  supplyAddress = address(42);
-    address private admin;
 
     constructor(
         address _owner,
-        address _admin,
         address _minter,
         string memory _name,
         string memory _symbol,
-        uint256 _initSupply,
         uint8 _decimals)
 
     public ERC20Detailed(_name, _symbol, _decimals) Ownable(_owner){
         minters[_minter] = _minter;
-        admin = _admin;
-        setTotalSupplyCap(_initSupply);
     }
 
     mapping(address=>address) public minters;
@@ -40,27 +34,11 @@ contract MineableToken is IMineableToken, ERC20, ERC20Detailed, Ownable {
         return (address(0) != minters[_minter]);
     }
 
-    function setTotalSupplyCap(uint256 newCap) public {
-        require(isOwner() || msg.sender == admin);
-
-        uint256 currentSupply = totalSupply();
-        require(newCap > currentSupply);
-
-        uint256 newTokenCount = newCap.sub(currentSupply);
-        _mint(supplyAddress, newTokenCount);
-    }
-
     function mint(address to, uint256 amount) public {
-        require(minters[msg.sender] != address(0), "not minter");
+        require(isMinter(msg.sender), "not minter");
         require(to != address(0), "can not mint to addresss 0");
 
-        _balances[supplyAddress] = _balances[supplyAddress].sub(amount, "ERC20: transfer amount exceeds balance");
-        _balances[to] = _balances[to].add(amount);
-        emit Transfer(supplyAddress, to, amount);
-    }
-
-    function setAdmin(address _newAdmin) public onlyOwner {
-        admin = _newAdmin;
+        _mint(to, amount);
     }
 
     function signature() external pure returns (string memory) {
