b072aed3b320a497a1e50b3aa69f3697abd25065
AKACoder <akacoder@outlook.com>
2020-10-17 20:06:46 +0800

    add platform fee withdraw function

--- adventure-island/contracts/AdventureIslandAdmin/AdventureIslandAdmin.sol
+++ adventure-island/contracts/AdventureIslandAdmin/AdventureIslandAdmin.sol
@@ -6,6 +6,9 @@ import "../AdventureIslandInternal/AdventureIslandInternal.sol";
 import "./AdventureIslandPoolManager.sol";
 
 contract AdventureIslandAdmin is AdventureIslandPoolManager {
+    using Address for address payable;
+    using SafeERC20 for IERC20;
+
     function setGlobalStartBlock(uint256 _newStart) external onlyAdmin {
         require(globalStartBlock > _newStart, "new start must less than current start");
         require(_newStart > block.number, "new start must after current block");
@@ -25,5 +28,29 @@ contract AdventureIslandAdmin is AdventureIslandPoolManager {
     function changeUniConnector(address _newUniConnector) external onlyAdmin {
         uniConnector = IUniswapConnector(_newUniConnector);
         ethPayer[_newUniConnector] = true;
+    }
+
+    function withdrawPlatformETHFeeTo(address payable _to, address _token) external onlyAdmin {
+        uint256 feeToWithdraw = platformFeeCollected[_token];
+        require(feeToWithdraw > 0, "platform fee is zero");
+
+        uint256 contractBalance = 0;
+        if(_token == ZERO_ADDRESS) {
+            contractBalance = address(this).balance;
+        } else {
+            contractBalance = IERC20(_token).balanceOf(address(this));
+        }
+
+        if(feeToWithdraw > contractBalance) {
+            feeToWithdraw = contractBalance;
+        }
+
+        if(_token == ZERO_ADDRESS) {
+            _to.sendValue(feeToWithdraw);
+        } else {
+            IERC20(_token).safeTransfer(_to, feeToWithdraw);
+        }
+
+        platformFeeCollected[_token] = 0;
     }
 }
