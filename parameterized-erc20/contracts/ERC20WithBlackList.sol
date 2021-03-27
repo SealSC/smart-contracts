8c41b8b32b6bd47aee9cb7801811205eabb2bffb
AKACoder <akacoder@outlook.com>
2021-03-27 11:57:12 +0800

    add black list opterations to parameterized erc20 contract

--- parameterized-erc20/contracts/ERC20WithBlackList.sol
+++ parameterized-erc20/contracts/ERC20WithBlackList.sol
@@ -1,9 +1,18 @@
 pragma solidity ^0.6.0;
 
 import "../../contract-libs/open-zeppelin/ERC20.sol";
+import "../../contract-libs/seal-sc/Simple3Role.sol";
 
-abstract contract ERC20WithBlackList is ERC20 {
+abstract contract ERC20WithBlackList is ERC20, Simple3Role {
     mapping(address=>bool) public blackList;
+
+    function addToBlackList(address _user) external onlyAdmin {
+        blackList[_user] = true;
+    }
+
+    function removeFromBlackList(address _user) external onlyAdmin {
+        blackList[_user] = false;
+    }
 
     function _transfer(address sender, address recipient, uint256 amount) internal override virtual {
         require(!blackList[sender], "blocked address");
