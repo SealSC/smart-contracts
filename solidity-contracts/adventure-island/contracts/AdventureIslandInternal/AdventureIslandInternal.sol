1a8ba1667afa877ef57c3d6ebdb0d7d2fe60a28a
AKACoder <akacoder@outlook.com>
2021-06-14 16:04:15 +0800

    remove files  not related to low-code-platform

--- solidity-contracts/adventure-island/contracts/AdventureIslandInternal/AdventureIslandInternal.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-
-pragma solidity ^0.6.0;
-
-import "./AdventureIslandPoolInternalOperations.sol";
-import "./AdventureIslandStakingOperations.sol";
-
-contract AdventureIslandInternal is AdventureIslandPoolInternalOperations {}
