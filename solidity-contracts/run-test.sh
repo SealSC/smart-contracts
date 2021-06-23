310663a5ae8a9ea12e1c828c81b6fbd2e24ce753
AKACoder <akacoder@outlook.com>
2021-06-23 19:20:43 +0800

    update readme & add test script of solidity contracts

---
+++ solidity-contracts/run-test.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+contractDIR=""
+openReport=$1
+
+truffle_exists() {
+    local ret='0'
+    truffle -v $1 >/dev/null 2>&1 || { local ret='1'; }
+
+    # fail on non-zero return value
+    if [ "$ret" -ne 0 ]; then
+      echo "truffle not install"
+      exit 1
+    fi
+
+    return 0
+}
+
+truffle_exists
+
+function runTest() {
+  echo ""
+  echo "-----> start ${contractDIR} testing"
+  echo "install ${contractDIR} testing dependencies"
+  cd $contractDIR
+  npm i > /dev/null 2>&1
+  echo "run ${contractDIR} test"
+  truffle run coverage > /dev/null 2>&1
+  echo "test report location: "
+  echo "${contractDIR}/coverage/lcov-report/index.html"
+
+  if [ "${openReport}" == "show" ]; then
+    open "coverage/lcov-report/index.html"
+  fi
+
+  cd ..
+}
+
+contractDIR="contract-deployer"
+runTest
+
+contractDIR="parameterized-erc20"
+runTest
+
+contractDIR="staking-mining"
+runTest
+
+contractDIR="uniswap-connector"
+runTest
