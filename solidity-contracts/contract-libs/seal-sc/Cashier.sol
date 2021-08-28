375cecec0a32ffe0a66a1adb2e7503e849ae3a39
AKACoder <akacoder@outlook.com>
2021-08-28 16:05:42 +0800

    add fee charge method to Cashier.sol

--- solidity-contracts/contract-libs/seal-sc/Cashier.sol
+++ solidity-contracts/contract-libs/seal-sc/Cashier.sol
@@ -11,11 +11,18 @@ contract Cashier is RejectDirectETH {
 
     uint256 constant public feeRatioBase = 1e18;
 
+    enum FEE_CHARGE_METHOD {
+        ByAmount,
+        ByRatio,
+        END
+    }
+
     struct FeeInfo {
         address currency;
         uint256 amount;
         uint256 ratio;
         address payable beneficiary;
+        FEE_CHARGE_METHOD chargeMethod;
         bool exists;
     }
 
@@ -23,7 +30,13 @@ contract Cashier is RejectDirectETH {
 
     event Received(address currency, address from, address to, uint256 amount);
 
-    function _setFeeInfo(uint256 _category, address _currency, uint256 _amount, uint256 _ratio, address payable _beneficiary) internal {
+    function _setFeeInfo(
+        uint256 _category,
+        address _currency,
+        uint256 _amount,
+        uint256 _ratio,
+        address payable _beneficiary,
+        FEE_CHARGE_METHOD _method) internal {
         require(_beneficiary != address(0), "beneficiary must not zero address");
 
         supportedFee[_category] = FeeInfo({
@@ -31,6 +44,7 @@ contract Cashier is RejectDirectETH {
             amount: _amount,
             ratio: _ratio,
             beneficiary: _beneficiary,
+            chargeMethod: _method,
             exists: true
         });
     }
@@ -40,6 +54,14 @@ contract Cashier is RejectDirectETH {
         require(supportedFee[_category].exists, "not supported fee");
 
         supportedFee[_category].beneficiary = _newBeneficiary;
+    }
+
+    function _changeFeeSetting(uint256 _category, FEE_CHARGE_METHOD _method, uint256 _newAmount, uint256 _newRatio) internal {
+        require(supportedFee[_category].exists, "not supported fee");
+
+        supportedFee[_category].chargeMethod = _method;
+        supportedFee[_category].amount = _newAmount;
+        supportedFee[_category].ratio = _newRatio;
     }
 
     function _removeFeeInfo(uint256 _category) internal {
@@ -63,15 +85,13 @@ contract Cashier is RejectDirectETH {
         return _amount;
     }
 
-    function _chargeFeeByAmount(uint256 _category, address _supplier) internal returns(uint256 feeCharged) {
+    function _charge(uint256 _category, address _supplier, uint256 _totalSupply) internal returns(uint256 feeCharged) {
         FeeInfo memory fi = supportedFee[_category];
-        return _chargeFee(fi, fi.amount, _supplier);
-    }
+        uint256 feeAmount = fi.amount;
+        if(fi.chargeMethod == FEE_CHARGE_METHOD.ByRatio) {
+            feeAmount = _totalSupply.percentageMul(fi.ratio, feeRatioBase);
+        }
 
-    function _chargeFeeByRatio(uint256 _category, uint256 _totalSupply, address _supplier) internal returns(uint256 feeCharged) {
-        FeeInfo memory fi = supportedFee[_category];
-        uint256 feeAmount = _totalSupply.percentageMul(fi.ratio, feeRatioBase);
         return _chargeFee(fi, feeAmount, _supplier);
     }
-
 }
